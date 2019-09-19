6e6b714a71c5daafefb1306a3288067c3cec3f73
Alexander Dmitriev <alex.golostos@gmail.com>
2019-09-20 00:37:23 +0400

    End of the lesson 1

---
+++ client/script.js
@@ -0,0 +1,29 @@
+// console.log("Hello")
+
+// $(function () {
+//     let description = $('#description');
+// })
+
+// window.onload = function() {
+//     let description = document.querySelector('#description');
+//     console.log(description.value);
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
+    let description = document.querySelector('#description');
+    console.log(description.value);
+})
+
+function nameOfFunc(arg1, arg2) {
+
+}
+
+let func2 = function(arg1, arg2, arg3) {
+
+}
+
+let func3 = (arg1, arg2, arg3) => { 
+
+}
+
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Regexp tests - web-development educational project
+
+Start growing up your knowledge in web-development!
+
+How to start:
+
+```shell
+npm i
+npm start
+```
