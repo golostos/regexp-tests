c062e471db4a85fab8e85887aa422b3eb20c0fdf
Alexander Dmitriev <alex.golostos@gmail.com>
2019-11-27 11:43:57 +0400

    Step 9

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import 'whatwg-fetch';
 
 import { getDataFromServer } from "./js-modules/get-data";
 import { createSamples } from "./js-modules/samples";
-// import './js-modules/test';
 
 document.addEventListener('DOMContentLoaded', () => {
     const regexp = document.querySelector('#regexp');
-    getDataFromServer('/api/tasks?id=2', (db) => {
+    const description = document.querySelector('#description');
+    console.log(window.location.pathname);
+    const taskId = /\/tasks\/(\d+)/.exec(window.location.pathname)[1] || 0;
+    getDataFromServer('/api/tasks/' + taskId, (db) => {
         regexp.value = db.regexp;
+        description.value = db.description;
         createSamples(db.samples);
     })
 })
---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const db = require('../db.json')
+const fs = require('fs')
+
+app.use(express.static('./client/'));
+app.use('/tasks/:id', express.static('./client/'));
+app.use(express.json())
+
+app.get('/api/tasks/:id', (req, res, next) => {
+    let tasksResponse;
+    if (db[req.params.id]) tasksResponse = db[req.params.id]
+    else tasksResponse = { error: 'Wrong Id' }
+    res.json(tasksResponse);
+})
+
+app.post('/api/newtask', (req, res, next) => {
+    createNewTask(req, res)
+})
+
+function createNewTask(req, res) {
+    db.push(req.body)
+    fs.writeFile('./db.json', JSON.stringify(db, null , 2), () => {
+        res.json({ status: "success" })
+    })
+}
+
+app.listen(port, () => {
+    console.log('Server started at http://localhost:' + port)
+})
