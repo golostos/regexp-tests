8cf32d962eb168f2b30c7c85c70e40bbce95e045
Alexander Dmitriev <alex.golostos@gmail.com>
2019-12-27 15:25:22 +0400

    Step 21

--- client/src/components/Samples.js
+++ client/src/components/Samples.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 export default function Samples(props) {
-  const samples = props.samples.map((el, index) => (
-    <div className="sample" key={index}>
-      <input className="input" type="text" placeholder="Sample" value={el} />
-      <div className="answer no-match"><span>Don't match</span></div>
-    </div>
+  const parent = React.createRef()
+    const samples = props.samples.map((el, index) => (
+      <Input key={index} el={el}/>
   ))
-  console.log(samples);
+  // componentDidMount, willUnmount, willUpdate
+  useEffect(() => {
+    const parentElem = parent.current;
+    parentElem.addEventListener('input', event => {
+      const regexpValue = document.querySelector('#regexp').value;
+      try {
+        const regexp = new RegExp('^' + regexpValue + '$');
+        const neib =  event.target.nextElementSibling;
+        if (regexp.test(event.target.value)) {
+          neib.classList.remove('no-match')
+          neib.classList.add('match')
+          neib.children[0].textContent = 'Match'
+        } else {
+          neib.classList.remove('match')
+          neib.classList.add('no-match')
+          neib.children[0].textContent = 'Don\'t match'
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })
+  }, [])
   return (
-    <div className="samples">
+    <div className="samples" ref={parent}>
       {samples}
+    </div>
+  )
+}
+
+function Input({el}) {
+  // const [inputText, setInputText] = useState(el)
+  return (
+    <div className="sample">
+      <input className="input" 
+        type="text" 
+        placeholder="Sample" 
+        defaultValue={el} 
+        // onChange={e => setInputText(e.target.value)}
+        />
+      <div className="answer no-match"><span>Don't match</span></div>
     </div>
   )
 }
